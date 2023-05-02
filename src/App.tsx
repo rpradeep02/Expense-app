f75025a48f27ab81f70f9fd061f8c4a4e2102f99
rpradeep02 <kumarrpradeep123@gmail.com>
2023-05-03 01:17:27 +0530

    expense filter added

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,30 @@
 import { useState } from "react";
 import ExpenseList from "./components/ExpenseList";
-// import Form from "./components/Form";
+import ExpenseFilter from "./components/ExpenseFilter";
 
+// import Form from "./components/Form";
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState("");
+
   const [expenses, setExpenses] = useState([
-    { id: 1, description: "aaa", amount: 10, category: "utilities" },
-    { id: 1, description: "bbb", amount: 10, category: "utilities" },
-    { id: 3, description: "ccc", amount: 10, category: "utilities" },
-    { id: 4, description: "ddd", amount: 10, category: "utilities" },
+    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
+    { id: 2, description: "bbb", amount: 10, category: "Utilities" },
+    { id: 3, description: "ccc", amount: 10, category: "Groceries" },
+    { id: 4, description: "ddd", amount: 10, category: "Groceries" },
   ]);
+
+  const visibleExpense = selectedCategory
+    ? expenses.filter((e) => e.category === selectedCategory)
+    : expenses;
   return (
     <div>
-      {/* <Form></Form> */}
+      <div className="mb-3">
+        <ExpenseFilter
+          onSelectedCategory={(category) => setSelectedCategory(category)}
+        />
+      </div>
       <ExpenseList
-        expenses={expenses}
+        expenses={visibleExpense}
         onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
       />
     </div>
---
+++ src/components/ExpenseFilter.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+interface Props {
+  onSelectedCategory: (category: string) => void;
+}
+
+const ExpenseFilter = ({ onSelectedCategory }: Props) => {
+  return (
+    <select
+      className="form-select"
+      onChange={(event) => onSelectedCategory(event.target.value)}
+    >
+      <option value="">All categories</option>
+      <option value="Groceries">Groceries</option>
+      <option value="Utilities">Utilities</option>
+      <option value="Entertainment">Entertainment</option>
+    </select>
+  );
+};
+
+export default ExpenseFilter;
